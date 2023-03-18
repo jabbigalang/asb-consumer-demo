17e3e64b4262b0266426235b9b1041186b782eaa
Chrisen Galang <chrisen.galang@gmail.com>
2023-03-18 18:19:45 +0800

    Setup service to receive messages from galang-queue

---
+++ src/main/java/com/chrisen/consumer/model/User.java
@@ -0,0 +1,14 @@
+package com.chrisen.consumer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String name;
+}
