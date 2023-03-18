9ee1421a2bc68a5e090d7c3dbd8b38b54c1e4614
Chrisen Galang <chrisen.galang@gmail.com>
2023-03-18 18:28:48 +0800

    Consume messages from galang-queue

---
+++ src/main/java/com/chrisen/consumer/messaging/ConsumerHello.java
@@ -0,0 +1,21 @@
+package com.chrisen.consumer.messaging;
+
+import com.chrisen.consumer.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
+
+@Component
+@Slf4j
+public class ConsumerHello {
+
+    @Bean
+    public Consumer<User> getUser() {
+        return message -> {
+            log.info("Hello " + message.getName() + "!");
+        };
+    }
+
+}
